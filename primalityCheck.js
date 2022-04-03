80b3f27b42e319d7ae32c48deb9aa07271accd3a
Emre ARSLAN <96077612+EmreArslan0101@users.noreply.github.com>
2022-04-03 12:12:32 +0300

    Add files via upload

--- primalityCheck.js
+++ primalityCheck.js
@@ -1,17 +1,17 @@
 const x = prompt("Type the number you want to check if it is prime")
-const asalDizisi =[]
+const primaryNumArr =[]
 
 for(let i = 1;i<=x; i++ ){
     if(x%i==0){
-        asalDizisi.push(Number(1))
+        primaryNumArr.push(Number(1))
     }else{
-        asalDizisi.push(Number(0))
+        primaryNumArr.push(Number(0))
     }
 }
 
-const bolen= asalDizisi.filter(x => x==1).length
+const divisor= primaryNumArr.filter(x => x==1).length
 
-if(bolen>2){
+if(divisor>2){
     console.log("This is not a prime number")
 }else{
     console.log("This is a prime number")
--- readme.md
+++ readme.md
@@ -1,2 +1,1 @@
-This javascript code has been written for websites, so this code is not
-functional in anywhere but DevTools console and Debugging console.
+This is a kind of function that checks primality status of number
