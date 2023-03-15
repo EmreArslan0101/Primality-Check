d3d43c6d6ce7d584465c04009e8c5f0012b33032
Emre ARSLAN <96077612+EmreArslan0101@users.noreply.github.com>
2023-03-15 22:22:24 +0300

    Update primalityCheck.js

--- primalityCheck.js
+++ primalityCheck.js
@@ -1,18 +1,15 @@
-const x = prompt("Type the number you want to check if it is prime")
-const primaryNumArr =[]
+const isPrime = function(num){
+    
+//If you want to use it, you can open it
+//const x = prompt("Type the number you want to check if it is prime");
+    
+    for(let i = 1;i<=x; i++ ){
+        if(num%i!=0){
+            return false;
+        };
+    };
+    return true;
+    
+};
 
-for(let i = 1;i<=x; i++ ){
-    if(x%i==0){
-        primaryNumArr.push(Number(1))
-    }else{
-        primaryNumArr.push(Number(0))
-    }
-}
-
-const divisor= primaryNumArr.filter(x => x==1).length
-
-if(divisor>2){
-    console.log("This is not a prime number")
-}else{
-    console.log("This is a prime number")
-}
+export isPrime;
